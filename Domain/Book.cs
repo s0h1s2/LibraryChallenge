409a70097b5ebd04abce158d25d59c83b5b275b5
shkar <shkarhsardar@gmail.com>
2025-07-08 13:34:35 +0300

    initial commit

---
+++ Domain/Book.cs
@@ -0,0 +1,36 @@
+namespace Domain;
+public class Book
+{
+    public string Isbn { get; private set; }
+    public string Title { get; private set; }
+    public CategoryId Category { get; private set; }
+    public string Author { get; private set; }
+    
+    public int AvailableCopies { get; private set; }
+    public DateTime DueDate { get; private set; }
+    private Book(string isbn, string title, CategoryId category, string author, int availableCopies)
+    {
+        Isbn = isbn;
+        Title = title;
+        Category = category;
+        Author = author;
+        AvailableCopies = availableCopies;
+    }
+
+    public static Book CreateBook(string isbn, string title, CategoryId category, string author,int availableCopies)
+    {
+        return new Book(isbn, title, category, author,availableCopies);
+    }
+
+    public void Borrow(DateTime dueDate)
+    {
+        if (this.AvailableCopies==0) throw new DomainException("Can't borrow book when no copies are available");
+        this.AvailableCopies--;
+        this.DueDate = dueDate;
+    }
+
+    public void Return()
+    {
+        this.AvailableCopies++;
+    }
+}
---
+++ Domain/DomainException.cs
@@ -0,0 +1,3 @@
+namespace Domain;
+
+public class DomainException(string message) : Exception(message);

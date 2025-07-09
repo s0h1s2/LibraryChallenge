5f47bdf533596baee69fcfcc800cbca2343a2209
shkar <shkarhsardar@gmail.com>
2025-07-09 13:33:34 +0300

    feat: add borrowing and returning functionality for books in BooksController and BookService

--- Web/Controllers/Messages.cs
+++ Web/Controllers/Messages.cs
@@ -5,5 +5,5 @@ public static class Messages
     public const string SuccessMessage = "Operation completed successfully.";
     public const string NotFoundMessage = "Resource not found.";
     public const string SearchTermNotFound = "Search  term cannot be empty.";
-    
+    public const string BookNotFound = "Book not found.";
 }

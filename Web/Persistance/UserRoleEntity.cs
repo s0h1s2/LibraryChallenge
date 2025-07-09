c267f13749de54ba09e17bdb52e1bdbd8d41c70e
shkar <shkarhsardar@gmail.com>
2025-07-09 20:43:31 +0300

    refactor: rename UserRoleId to Id in UserRoleEntity for consistency

--- Web/Persistance/UserRoleEntity.cs
+++ Web/Persistance/UserRoleEntity.cs
@@ -2,7 +2,7 @@ namespace Web.Persistance;
 
 public class UserRoleEntity
 {
-    public int UserRoleId { get; set; }
+    public int Id { get; set; }
     public int UserId { get; set; }
     public int RoleId { get; set; }
     public UserEntity User { get; set; } = null!;

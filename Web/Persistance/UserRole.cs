cd064f412098783a2802f7d4da44adb856c0b442
shkar <shkarhsardar@gmail.com>
2025-07-09 20:37:46 +0300

    feat: add RoleEntity with permissions and UserRole entity for role management

---
+++ Web/Persistance/UserRole.cs
@@ -0,0 +1,10 @@
+namespace Web.Persistance;
+
+public class UserRole
+{
+    public int UserRoleId { get; set; }
+    public int UserId { get; set; }
+    public int RoleId { get; set; }
+    public UserEntity User { get; set; } = null!;
+    public RoleEntity Role { get; set; } = null!;
+}

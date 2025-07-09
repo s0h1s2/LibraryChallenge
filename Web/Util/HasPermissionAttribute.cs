a20a3ae002e824313bbbcb24af93e85e8afd9eeb
shkar <shkarhsardar@gmail.com>
2025-07-09 20:12:14 +0300

    feat: add HasPermissionAttribute for custom authorization policies

---
+++ Web/Util/HasPermissionAttribute.cs
@@ -0,0 +1,14 @@
+using Core.ValueObjects;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Web.Util;
+
+public class HasPermissionAttribute:AuthorizeAttribute
+{
+    public HasPermissionAttribute(AttributeType attributeType):base(policy:attributeType.ToString())
+    {
+    }
+    
+    
+}

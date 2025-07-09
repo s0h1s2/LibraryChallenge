1ddd70e2221e2c1ab0c81979f596ff573d8472d3
shkar <shkarhsardar@gmail.com>
2025-07-09 19:55:32 +0300

    feat: implement user creation in UsersController with success response

--- Web/Controllers/BaseController.cs
+++ Web/Controllers/BaseController.cs
@@ -1,3 +1,4 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers;
@@ -5,7 +6,7 @@ namespace Web.Controllers;
 public class BaseController:ControllerBase
 {
     protected IActionResult Success<T>(T? data, string? message = null,int? status= 200)
-        => Ok(ApiResponse<T>.Ok(data, message));
+        => StatusCode(status??200,ApiResponse<T>.Ok(data, message));
 
     protected IActionResult Failure(string message, int statusCode = 400)
         => StatusCode(statusCode, ApiResponse<string>.Fail(message, statusCode));
--- Web/Controllers/UsersController.cs
+++ Web/Controllers/UsersController.cs
@@ -1,16 +1,34 @@
+using System.Net;
 using Core.Dto;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web.Persistance;
 
 namespace Web.Controllers;
 [ApiController]
 [Route("api/v1/[controller]")]
 public class UsersController:BaseController
 {
-    [HttpPost("")]
-    public IActionResult CreateUser([FromBody] CreateUser createUser)
+    private readonly ApplicationDbContext _dbContext;
+
+    public UsersController(ApplicationDbContext dbContext)
     {
-        
-        return Ok(new { Message = "User created successfully" });
+        _dbContext = dbContext;
+    }
+    [HttpPost("")]
+    public async Task<IActionResult> CreateUser([FromBody] CreateUser createUser)
+    {
+        _dbContext.User.Add(new UserEntity()
+        {
+            Email = createUser.Email,
+            PasswordHash = new PasswordHasher<object?>().HashPassword(null,createUser.Password)
+        });
+        await _dbContext.SaveChangesAsync();
+        return Success(new
+        {
+            Email = createUser.Email,
+        },status:201);
     }
     
 }

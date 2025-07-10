97ecdb113a4214cb49413594ff1c0f14df960c7b
shkar <shkarhsardar@gmail.com>
2025-07-10 11:33:22 +0300

    refactor: streamline data seeding process by dynamically invoking seeder configurations

--- Web/Persistance/Seeding/DataSeeder.cs
+++ Web/Persistance/Seeding/DataSeeder.cs
@@ -4,12 +4,18 @@ namespace Web.Persistance.Seeding;
 
 public class DataSeeder
 {
+    
     public static void SeedData(ModelBuilder modelBuilder)
     {
-        CategorySeeder.Configure(modelBuilder);
-        BookSeeder.Configure(modelBuilder);
-        RoleSeeder.Configure(modelBuilder);
-        PermissionSeeder.Configure(modelBuilder);
-        UserSeeder.Configure(modelBuilder);
+        var seederType = typeof(IDataSeeder);
+        var seeders = typeof(DataSeeder).Assembly
+            .GetTypes()
+            .Where(t => seederType.IsAssignableFrom(t) && !t.IsInterface && !t.IsAbstract);
+
+        foreach (var seeder in seeders)
+        {
+            var method = seeder.GetMethod("Configure", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+            method?.Invoke(null, new object[] { modelBuilder });
+        }
     }
 }

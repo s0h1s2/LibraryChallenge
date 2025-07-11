e3f29ddec139eeeeeb4589c0bda4159968061446
shkar <shkarhsardar@gmail.com>
2025-07-11 13:34:20 +0300

    test: refactor UserTest to use test fixture with IDisposable for improved setup and disposal logic
    Signed-off-by: shkar <shkarhsardar@gmail.com>

--- Core.UnitTests/UserTest.cs
+++ Core.UnitTests/UserTest.cs
@@ -3,26 +3,31 @@ using Core.ValueObjects;
 
 namespace Core.UnitTests;
 
-public class UserTest
+public class UserTest : IDisposable
 {
+    private User _user;
+
+    public UserTest()
+    {
+        _user = User.Create("user@mail.com", "password123", 1);
+    }
+
+    public void Dispose()
+    {
+    }
+
     [Fact]
     public void TestAssignRole_To_User_User_Must_Have_Role()
     {
-        // Arrange
-        var user = User.Create("user@mail.com", "password123", 1);
         var role = Role.Create(RoleType.Admin);
-        user.AssignRole(role);
-        // Assert
-        Assert.Equal(role, user.Role);
+        _user.AssignRole(role);
+        Assert.Equal(role, _user.Role);
     }
 
     [Fact]
     public void TestAssignRole_To_User_User_Must_Throw_Exception_When_Role_Is_Null()
     {
-        // Arrange
-        var user = User.Create("user@mail.com", "password123", 1);
         Role role = null;
-        // Act & Assert
-        Assert.Throws<DomainException>(() => user.AssignRole(role));
+        Assert.Throws<DomainException>(() => _user.AssignRole(role));
     }
 }

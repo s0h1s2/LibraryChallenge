1996ade3d452e4d97e38324f5ab4f0d87e126c9f
shkar <shkarhsardar@gmail.com>
2025-07-12 17:22:17 +0300

    feat: add `RoleManagementController` with `ManageRolePermissions` endpoint, introduce `ManageRolePermissionsValidation` for request validation, and enhance resource update messaging in `Messages`
    Signed-off-by: shkar <shkarhsardar@gmail.com>

---
+++ Web/Validations/ManageRolePermissionsValidation.cs
@@ -0,0 +1,29 @@
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Web.Controllers;
+using Web.Persistence;
+
+namespace Web.Validations;
+
+public class ManageRolePermissionsValidation : AbstractValidator<RoleManagementController.ManageRolePermissionsRequest>
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public ManageRolePermissionsValidation(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        RuleFor(x => x.AllowedPermissions)
+            .MustAsync(CheckIfPermissionsExist)
+            .WithMessage("Allowed permissions must be valid and exist in the system.");
+        RuleFor(x => x.DeniedPermissions)
+            .MustAsync(CheckIfPermissionsExist)
+            .WithMessage("Denied permissions must be valid and exist in the system.");
+    }
+
+    private async Task<bool> CheckIfPermissionsExist(IEnumerable<string> rolePermissions,
+        CancellationToken cancellationToken)
+    {
+        if (!rolePermissions.Any()) return true;
+        return (await _dbContext.Permission.ToListAsync(cancellationToken)).Any(x => rolePermissions.Contains(x.Name));
+    }
+}

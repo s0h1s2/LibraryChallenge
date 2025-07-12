ad464c2f1bb2477c29841e57764bacd8c7a102da
shkar <shkarhsardar@gmail.com>
2025-07-12 10:49:54 +0300

    refactor: introduce `CustomValidationErrorResponse` to standardize validation error formatting and integrate it with FluentValidation configuration
    Signed-off-by: shkar <shkarhsardar@gmail.com>

---
+++ Web/CustomValidationErrorResponse.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using SharpGrip.FluentValidation.AutoValidation.Mvc.Results;
+
+public class CustomValidationErrorResponse : IFluentValidationAutoValidationResultFactory
+{
+    public IActionResult CreateActionResult(ActionExecutingContext context,
+        ValidationProblemDetails? validationProblemDetails)
+    {
+        var errors = validationProblemDetails.Errors
+            .Select(pair => new KeyValuePair<string, string[]>(pair.Key.ToLower(), pair.Value))
+            .ToDictionary(t => t.Key.ToLower(), t => t.Value);
+        var errorsResult = new ValidationProblemDetails
+        {
+            Errors = errors,
+            Title = validationProblemDetails.Title,
+            Detail = validationProblemDetails.Detail,
+            Extensions = validationProblemDetails.Extensions,
+            Instance = validationProblemDetails.Instance,
+            Status = StatusCodes.Status422UnprocessableEntity,
+            Type = validationProblemDetails.Type
+        };
+        return new UnprocessableEntityObjectResult(errorsResult);
+    }
+}

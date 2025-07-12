146cc75501da484f1d2ad31aa975e9b48d2da3c5
shkar <shkarhsardar@gmail.com>
2025-07-12 19:47:07 +0300

    refactor: replace `IBookRepository` methods with `DbContext` in `BookService`, delegate borrow/return logic to `LibraryService`, and remove redundant repository dependencies
    Signed-off-by: shkar <shkarhsardar@gmail.com>

--- Core/Persistance/IBookRepository.cs
+++ Core/Persistance/IBookRepository.cs
@@ -1,4 +1,3 @@
-using Core.Dto;
 using Core.Entity;
 
 namespace Core.Persistance;
@@ -7,9 +6,7 @@ public interface IBookRepository
 {
     Task<IList<Book>> GetBooksAsync();
     Task<Book?> GetBookByIdAsync(Guid id);
-    Task<Book?> AddBookAsync(CreateBook book);
     Task<Book> UpdateBookAsync(Book book);
     Task<bool> DeleteBookAsync(Guid id);
     Task<IEnumerable<Book>> FilterBooksAsync(string searchTerm);
-    Task UpdateBookAndBorrowedBooksByUserAsync(Book book, User user);
 }
--- Web/Services/BookService.cs
+++ Web/Services/BookService.cs
@@ -1,42 +1,49 @@
 using Core.Dto;
 using Core.Entity;
-using Core.Persistance;
+using Core.Services;
+using Microsoft.EntityFrameworkCore;
+using Web.Persistence;
 
 namespace Web.Services;
 
 public class BookService
 {
-    private readonly IBookRepository _bookRepository;
-    private readonly IUserRepository _userRepository;
+    private readonly LibraryService _libraryService;
+    private ApplicationDbContext _dbContext;
 
-    public BookService(IBookRepository bookRepository, IUserRepository userRepository)
+    public BookService(LibraryService libraryService, ApplicationDbContext dbContext)
     {
-        _bookRepository = bookRepository;
-        _userRepository = userRepository;
+        _libraryService = libraryService;
+        _dbContext = dbContext;
     }
 
-    public async Task<Book?> AddBookAsync(CreateBook book)
+    public async Task<Book?> AddBookAsync(CreateBook bookToAdd)
     {
-        return await _bookRepository.AddBookAsync(book);
+        var book = _dbContext.Books.Add(bookToAdd.ToBook());
+        await _dbContext.SaveChangesAsync();
+        return book.Entity;
     }
 
     public async Task BorrowBookAsync(BorrowBook borrowBook, Guid userId, DateTime dueDate)
     {
-        var book = await _bookRepository.GetBookByIdAsync(borrowBook.BookId);
-        var user = await _userRepository.GetUserByIdAsync(userId);
+        var book = await _dbContext.Books.FindAsync(borrowBook.BookId);
+        var user = await _dbContext.User.Include(user => user.BorrowedBooks).FirstAsync(user => user.Id == userId);
         if (book is null || user is null) throw new KeyNotFoundException();
-        book.Borrow();
-        user.AddBookToBorrowedBooks(book, dueDate);
-        await _bookRepository.UpdateBookAndBorrowedBooksByUserAsync(book, user);
+
+        _libraryService.BorrowBook(book, user, dueDate);
+        _dbContext.Add(book);
+        _dbContext.Add(user.BorrowedBooks);
+        await _dbContext.SaveChangesAsync();
     }
 
     public async Task ReturnBookAsync(Guid bookId, Guid userId)
     {
-        var book = await _bookRepository.GetBookByIdAsync(bookId);
-        var user = await _userRepository.GetUserWithBorrowedBooksAsync(userId, bookId);
+        var book = await _dbContext.Books.FindAsync(bookId);
+        var user = await _dbContext.User.Include(user => user.BorrowedBooks).FirstAsync(user => user.Id == userId);
         if (book is null || user is null) throw new KeyNotFoundException();
-        book.Return();
-        user.MarkBookAsReturned(book);
-        await _bookRepository.UpdateBookAndBorrowedBooksByUserAsync(book, user);
+        _libraryService.ReturnBook(book, user);
+        _dbContext.Books.Update(book);
+        _dbContext.BorrowedBooks.UpdateRange(user.BorrowedBooks);
+        await _dbContext.SaveChangesAsync();
     }
 }
